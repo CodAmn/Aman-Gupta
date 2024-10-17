a79bdd7d56866518daa8a090cc26e8ae33f5436a
CodAmn <150777995+CodAmn@users.noreply.github.com>
2024-10-17 14:21:09 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no of a");
+
+        int a = sc.nextInt();
+        System.out.println("enter no. of b");
+        int b = sc.nextInt();{
+            System.out.println("sum = "+( a+b));}
+
+    }
+}
